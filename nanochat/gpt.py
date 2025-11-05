3f3dc2c495e860658b0a445503f7a752e69da212
Blake Fullerton <fullerton199@gmail.com>
2025-11-04 20:26:43 -0800

    adding more to self attention

--- nanochat/gpt.py
+++ nanochat/gpt.py
@@ -29,3 +29,27 @@ def norm(x):
     #purely functoinal rmsnorm with no learnable params
     return F.rms_norm(x, (x.size(-1),))
 
+def apply_rotary_emb(x, cos, sin):
+    assert x.ndim == 4 #multi head attention
+    d = x.shape[3] // 2
+    x1, x2 = x[..., :d], x[..., d:] # split up into last time into two halves
+    y1 = x1 * cos + x2 * sin #rotate paids of dims
+    y2 = x1 * (-sin) + x2 * cos
+    out = torch.cat([y1,y2], 3) # re aseemble
+    out = out.to(x.dtype) #ensure input / output d types match
+
+
+class CasualSelfAttention(nn.module):
+    def __init__(self, config, layer_idx):
+        super().__init__()
+        self.layer_idx = layer_idx
+        self.n_head = config.n_head
+        self.n_kv_head = config.n_kv_head
+        self.n_embd = config.n_embd
+        self.head_dim = self.n_embd // self.n_head
+        assert self.n_embd % self.n_head == 0
+        assert self.n_kv_head <= self.n_head and self.n_head % self.n_kv_head == 0
+        self.c_q = nn.Linear(self.n_embd, self.n_head * self.head_dim, bias=False)
+        self.c_k = nn.Linear(self.n_embd, self.n_kv_head * self.head_dim, bias=False)
+        self.c_v = nn.Linear(self.n_embd, self.n_kv_head * self.head_dim, bias=False)
+        self.c_proj = nn.Linear(self.n_embd, self.n_embd, bias=False)
